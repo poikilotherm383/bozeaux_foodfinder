4686061732ea237cc999e0d6d827c960a364da13
poikilotherm383 <hudson.horne.318@gmail.com>
2026-03-07 12:41:45 -0500

    bolstered parsing logic, added expanded search, and implemented Chick-Fil-A feature

--- src/app.py
+++ src/app.py
@@ -6,42 +6,55 @@ from maps_api import MapsClient
 import json
 
 config_path = os.path.join(os.path.dirname(__file__), "config", "config.json")
-static_folder = os.path.join(os.path.dirname(__file__), "static")
-
 with open(config_path, mode = "r") as file:
     config = json.load(file)
 static_url_path = config.get("static_url_path")
 
-app = Flask(__name__, static_url_path = "/static")
-
+app = Flask(__name__)
 maps_client = MapsClient(config_path)
 
 # flask theme selection: chooses light mode or dark mode
 @app.route("/")
 def home():
-
     hour = datetime.now().hour
-
     if 7 <= hour <= 19:
         theme = "light"
     else:
         theme = "dark"
-
-    return render_template("index.html", theme=theme, url_root = static_url_path)
+    return render_template("index.html", theme = theme, url_root = static_url_path)
 
 # flask search endpoint    
-@app.route("/search", methods=["POST"])
+@app.route("/search", methods = ["POST"])
 def search():
-
     data = request.json
+    place_type = data.get("type")
+    lat = data.get("lat")
+    lon = data.get("lon")
+    if any([val is None for val in [place_type, lat, lon]]):
+        return jsonify({"message": "Bad request format"}), 400
+    try:
+        results, radius = maps_client.search(lat, lon, place_type)
+    except Exception as e:
+        return jsonify({"message": e}), 500
+    if len(results) == 0:
+        return jsonify({"message": f"No places found within {radius:.0f} miles", "empty": True}), 404
+    return jsonify(results)
 
-    place_type = data["type"]
-    lat = data["lat"]
-    lon = data["lon"]
-
-    results = maps_client.search(lat, lon, place_type)
-
+@app.route("/search_expand", methods = ["POST"])
+def search_expand():
+    data = request.json
+    place_type = data.get("type")
+    lat = data.get("lat")
+    lon = data.get("lon")
+    if any([val is None for val in [place_type, lat, lon]]):
+        return jsonify({"message": "Bad request format"}), 400
+    try:
+        results, radius = maps_client.search_expand(lat, lon, place_type)
+    except Exception as e:
+        return jsonify({"message": e}), 500
+    if len(results) == 0:
+        return jsonify({"message": f"No places found within {radius:.0f} miles", "empty": True}), 404
     return jsonify(results)
 
 if __name__ == "__main__":
-    app.run(host = "0.0.0.0", port = 5050)
+    app.run(host = "127.0.0.1", port = 5050)
--- src/maps_api.py
+++ src/maps_api.py
@@ -1,12 +1,23 @@
 import requests
-import os
 import json
 from typing import Literal
 from math import pi, cos, sqrt
 import time
 
+def miles_to_m(miles: float):
+    return miles * 1.60934e3
+
 class MapsClient:
     url = "https://places.googleapis.com/v1/places:searchNearby"
+    day_dict = {
+        0: "Sunday",
+        1: "Monday",
+        2: "Tuesday",
+        3: "Wednesday",
+        4: "Thursday",
+        5: "Friday",
+        6: "Saturday"
+    }
     def __init__(self, config_path):
         self.api_key = self._load_api_key(config_path)
 
@@ -16,7 +27,7 @@ class MapsClient:
         return self.config_data.get("api_key")
     
     def _calculate_distance(self, lat: float, lon: float, userlat: float, userlon: float):
-        if lat is None or lon is None:
+        if (lat is None) or (lon is None):
             return None
         dlat = abs(lat - userlat) * pi / 180
         dlon = abs(lon - userlon) * pi / 180
@@ -24,19 +35,35 @@ class MapsClient:
         return d
     
     def _find_next_closing(self, hours: dict):
+        if not isinstance(hours, dict):
+            return None
         ctime = time.localtime().tm_mday + time.localtime().tm_hour / 24 + time.localtime().tm_min / (24 * 60)
-        closings = [
-            {
-                "hour": period["close"]["hour"],
-                "minute": period["close"]["minute"],
-                "timedelta": period["close"]["date"]["day"] + period["close"]["hour"] / 24 + period["close"]["minute"] / (24 * 60) - ctime
-            }
-            for period in hours
-        ]
+        today = time.localtime().tm_mday
+        try:
+            closings = [
+                {
+                    "hour": period["close"]["hour"],
+                    "minute": period["close"]["minute"],
+                    "day": period["close"]["date"]["day"],
+                    "weekday": period["close"]["day"],
+                    "timedelta": period["close"]["date"]["day"] + period["close"]["hour"] / 24 + period["close"]["minute"] / (24 * 60) - ctime
+                }
+                for period in hours
+            ]
+        except KeyError:
+            return None
         closings.sort(key = lambda x: x["timedelta"])
         hour = closings[0]["hour"]
         minute = closings[0]["minute"]
-        return f"""{hour - 12 if hour > 12 else hour}:{minute} {'PM' if hour // 12 == 1 else 'AM'}"""
+        day = closings[0]["day"]
+        match (day - today):
+            case 0:
+                weekday = "today"
+            case 1:
+                weekday = "tomorrow"
+            case _:
+                weekday = self.day_dict[closings[0]["weekday"]]
+        return f"""{hour - 12 if hour > 12 else hour}:{minute} {'PM' if hour // 12 == 1 else 'AM'} {weekday}"""
     
     def _parse_results(self, results: dict, userlat: float, userlon: float):
         open_results = [result for result in results if (result.get("currentOpeningHours") and result["currentOpeningHours"].get("openNow"))]
@@ -50,28 +77,48 @@ class MapsClient:
                 "distance": distance,
                 "address": result.get("formattedAddress"),
                 "url": result.get("websiteUri"),
-                "closes_at": self._find_next_closing(result.get("currentOpeningHours").get("periods")),
+                "closes_at": self._find_next_closing(result.get("currentOpeningHours").get("periods")) if result.get("currentOpeningHours") else None,
                 "lat": lat,
                 "lon": lon
                 })
         return parsed_results
     
-    def search(self, lat: float, lon: float, type: Literal ["coffee_shop", "restaurant", "chick_fil_a"]):
-        if type not in ["coffee_shop", "restaurant"]:
-            raise Exception(f"Unknown type {type}")
-        payload = {
-            "includedPrimaryTypes": [type],
-            "maxResultCount": 20,
-            "rankPreference": "DISTANCE",
-            "locationRestriction": {
-                "circle": {
-                    "center": {
-                        "latitude": lat,
-                        "longitude": lon},
-                "radius": 3000.0
+    def search(self, lat: float, lon: float, type: Literal ["coffee_shop", "restaurant", "chick_fil_a"], radius : float = miles_to_m(1)):
+        if type in ["coffee_shop", "restaurant"]:
+            payload = {
+                "includedPrimaryTypes": [type],
+                "maxResultCount": 20,
+                "rankPreference": "DISTANCE",
+                "locationRestriction": {
+                    "circle": {
+                        "center": {
+                            "latitude": lat,
+                            "longitude": lon
+                        },
+                    "radius": radius
                     }
                 }
             }
+        elif type in ["chick_fil_a"]:
+            payload = {
+                "textQuery": "Chick-Fil-A",
+                "includedType": "restaurant",
+                "pageSize": 5,
+                "strictTypeFiltering": True,
+                "openNow": True,
+                "rankPreference": "DISTANCE",
+                "locationBias": {
+                    "circle": {
+                        "center": {
+                            "latitude": lat,
+                            "longitude": lon,
+                        },
+                        "radius": radius
+                    }
+                }
+            }
+        else:
+            raise Exception(f"Unsupported type {type}")
         headers = {
             "Content-Type": "application/json",
             "X-Goog-Api-Key": self.api_key, # insert api key here
@@ -82,8 +129,16 @@ class MapsClient:
             headers = headers,
             json = payload
         )
-        if not response.status_code == 200:
+        if response.status_code != 200:
             raise Exception(f"Maps API returned status code {response.status_code}, full response {response.text}")
         response = response.json()
         parsed_response = self._parse_results(response.get("places", []), lat, lon)
-        return parsed_response
+        return (parsed_response, radius)
+    # searches with an expanding radius 
+    def search_expand(self, lat: float, lon: float, type: Literal ["coffee_shop", "restaurant", "chick_fil_a"]):
+        search_radii = [miles_to_m(2), miles_to_m(5), miles_to_m(10), miles_to_m(20)]
+        for radius in search_radii:
+            results = self.search(lat, lon, type, radius = radius)
+            if len(results) >= 5:
+                break
+        return (results, radius)
